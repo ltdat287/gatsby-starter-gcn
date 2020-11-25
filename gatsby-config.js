8595cdafd44285164e5519aa203430b9f0f2781d
cmc-datlt <ltdat4@cmc.com.vn>
2020-11-25 17:23:40 +0700

    add bootstrap4 starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Source Sans Pro\:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i`,
+          `Merriweather\:300,300i,400,400i,700,700i,900,900i`,
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint:
+          "https://gmail.us7.list-manage.com/subscribe/post?u=40f7fdece7362ae9a7ec65f78&amp;id=18d4ff27aa",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,67 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FaTwitter, FaFacebookF, FaInstagram } from "react-icons/fa"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Newsletter from "../components/newsletter"
+
+const bgVideo = require("../dist/mp4/bg.mp4")
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+    <div className="overlay"></div>
+    <video
+      playsInline="playsInline"
+      autoPlay="autoPlay"
+      muted="muted"
+      loop="loop"
+    >
+      <source src={bgVideo} type="video/mp4"></source>
+    </video>
+
+    <div className="masthead">
+      <div className="masthead-bg"></div>
+      <div className="container h-100">
+        <div className="row h-100">
+          <div className="col-12 my-auto">
+            <div className="masthead-content text-white py-5 py-md-0">
+              <h1 className="mb-3">Coming Soon!</h1>
+              <p className="mb-5">
+                We're working hard to finish the development of this site. Our
+                target launch date is
+                <strong>January 2022</strong>! Sign up for updates using the
+                form below!
+              </p>
+              <Newsletter />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+    <div className="social-icons">
+      <ul className="list-unstyled text-center mb-0">
+        <li className="list-unstyled-item">
+          <a href="#">
+            <FaTwitter />
+          </a>
+        </li>
+        <li className="list-unstyled-item">
+          <a href="#">
+            <FaFacebookF />
+          </a>
+        </li>
+        <li className="list-unstyled-item">
+          <a href="#">
+            <FaInstagram />
+          </a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 )
 
